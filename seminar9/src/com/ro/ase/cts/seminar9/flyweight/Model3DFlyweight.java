cf17a74fa10aa13e597c50a29e706a0e30be8e16
irinelenache <irinel@themeisle.com>
2021-04-22 09:21:36 +0300

    seminar 9 final

---
+++ seminar9/src/com/ro/ase/cts/seminar9/flyweight/Model3DFlyweight.java
@@ -0,0 +1,30 @@
+package com.ro.ase.cts.seminar9.flyweight;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Model3DFlyweight implements ModelFlyweightInterface{
+
+	ArrayList<Double> modelPoints = new ArrayList<>();
+	String nume;
+	
+	public Model3DFlyweight(String nume) {
+		super();
+		this.nume = nume;
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		this.modelPoints = new ArrayList<>(new Random().nextInt(1000));
+	}
+
+	@Override
+	public void display(ScreenData data) {
+		// TODO Auto-generated method stub
+		System.out.println(String.format("%s at coordinates (%f,%f,%f) with color: %s", 
+				this.nume, data.getX(), data.getY(), data.getZ(), data.getColor()));
+	}
+
+}
--- seminar9/src/com/ro/ase/cts/seminar9/proxy/YoutubeLibraryProxy.java
+++ seminar9/src/com/ro/ase/cts/seminar9/proxy/YoutubeLibraryProxy.java
@@ -1,21 +1,34 @@
 package com.ro.ase.cts.seminar9.proxy;
 
+import java.util.HashMap;
+
 public class YoutubeLibraryProxy implements YoutubeLibraryInterface{
 
 	private YoutubeLibraryInterface libraryImplementation;
+	private String videoListCached;
+	private HashMap<Integer,String> videoDescriptionsCached;
+	private boolean hasExpired;
+	
 	public YoutubeLibraryProxy() {
+		hasExpired = false;
 		libraryImplementation = new YoutubeLibrary();
+		videoDescriptionsCached = new HashMap<>();
 	}
 	@Override
 	public String listVideos() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		if(videoListCached == null || hasExpired) {
+			videoListCached = libraryImplementation.listVideos();
+		}
+		return videoListCached;
 	}
 
 	@Override
 	public String getVideoInfo(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		if(videoDescriptionsCached.get(id) == null || hasExpired) {
+			videoDescriptionsCached.put(id,libraryImplementation.getVideoInfo(id));
+		}
+		return videoDescriptionsCached.get(id);
 	}
 	
 }
